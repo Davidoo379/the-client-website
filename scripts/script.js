049e35e4dfd26ccc1bbcb9e5ad77bd37c007b742
David Jak <david.jak@student.hku.nl>
2026-09-23 18:03:47 +0200

    Changed html
    better symantics and unnececary elements

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-async function loadPartial(id, url) {
-    const res = await fetch(url);
-    document.getElementById(id).innerHTML = await res.text();
-}
-loadPartial('colleague-card', '/cards/colleague-card.html');
-loadPartial('event-card', '/cards/event-card.html');
-loadPartial('open-question', '/cards/open-question-card.html');
-loadPartial('opportunitie-card', '/cards/opportunitie-card.html');
