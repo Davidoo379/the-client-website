c9d7c727ac3f552ff9b64d32fddd37d4da70cb26
David Jak <davidogameplay@gmail.com>
2026-09-24 16:23:13 +0200

    two content pages done html

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+// filters.js
+function submitOpportunitiesFilters() {
+    document.getElementById('type-of-post').requestSubmit();
+    document.getElementById('role').requestSubmit();
+    document.getElementById('research-type').requestSubmit();
+    document.getElementById('sub-specialism').requestSubmit();
+    document.getElementById('hospital-or-research-institude').requestSubmit();
+    document.getElementById('mesh-terms').requestSubmit();
+    document.getElementById('intentions').requestSubmit();
+}
+function submitEventsAndConventionsFilters() {
+    document.getElementById('type-of-event').requestSubmit();
+    document.getElementById('distance').requestSubmit();
+    document.getElementById('hospital-or-research-institude').requestSubmit();
+    document.getElementById('mesh-terms').requestSubmit();
+}
