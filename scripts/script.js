f4990c7b0bf59246db34af55ccf167238d5719e7
David Jak <davidogameplay@gmail.com>
2026-09-24 16:49:11 +0200

    Final content page html

--- scripts/script.js
+++ scripts/script.js
@@ -4,13 +4,21 @@ function submitOpportunitiesFilters() {
     document.getElementById('role').requestSubmit();
     document.getElementById('research-type').requestSubmit();
     document.getElementById('sub-specialism').requestSubmit();
-    document.getElementById('hospital-or-research-institude').requestSubmit();
+    document.getElementById('hospital-or-research-institute').requestSubmit();
     document.getElementById('mesh-terms').requestSubmit();
     document.getElementById('intentions').requestSubmit();
 }
 function submitEventsAndConventionsFilters() {
     document.getElementById('type-of-event').requestSubmit();
     document.getElementById('distance').requestSubmit();
-    document.getElementById('hospital-or-research-institude').requestSubmit();
+    document.getElementById('hospital-or-research-institute').requestSubmit();
     document.getElementById('mesh-terms').requestSubmit();
+}
+function submitColleaguesFilters() {
+    document.getElementById('role').requestSubmit();
+    document.getElementById('research-type').requestSubmit();
+    document.getElementById('sub-specialism').requestSubmit();
+    document.getElementById('hospital-or-research-institute').requestSubmit();
+    document.getElementById('mesh-terms').requestSubmit();
+    document.getElementById('intentions').requestSubmit();
 }
